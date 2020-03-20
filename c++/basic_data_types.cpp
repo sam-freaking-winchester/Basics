325d34e5b4fc40b67e19a4813d8c764ac1ff8055
sam.freaking.winchester <samwinchester.aka.moose@gmail.com>
2020-03-20 21:20:54 +0530

    Adding C++ Folder

---
+++ c++/basic_data_types.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main()
+{
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    scanf("%d %ld %c %f %lf",&a,&b,&c,&d,&e );
+    printf("%d %ld %c %f %lf",a,b,c,d,e );
+    return 0;
+}
